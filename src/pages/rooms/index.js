d2504db8dfb3221b13d073e984742974a399f28a
kansodaria <sh788512@dal.ca>
2022-07-18 05:17:15 -0300

    Chatbot initial stage with room booking

--- src/pages/rooms/index.js
+++ src/pages/rooms/index.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Grid, Link } from '@mui/material';
-import ListView from '../../components/listview';
+import { Grid, Link } from "@mui/material";
+import ListView from "../../components/listview";
 
-import AXIOS_CLIENT from '../../utils/api-client';
+import AXIOS_CLIENT from "../../utils/api-client";
 
 const RoomList = () => {
   const [roomList, setRoomList] = useState(null);
   useEffect(() => {
-    AXIOS_CLIENT.post('api/hotel/getrooms', { path: 'getrooms' })
+    AXIOS_CLIENT.post("api/hotel/getrooms", { path: "getrooms" })
       .then((res) => {
         if (res.status === 200) {
           setRoomList(JSON.parse(res.data.body));
@@ -20,15 +20,17 @@ const RoomList = () => {
 
   const navigate = useNavigate();
   const handleRooms = () => {
-    navigate('/hotel');
+    navigate("/hotel");
   };
 
   return (
     <div>
-      <Grid item>
-        <Link onClick={handleRooms} variant="body2">
-          {'Bookings'}
-        </Link>
+      <Grid Container>
+        <Grid item>
+          <Link onClick={handleRooms} variant="body2">
+            {"Bookings"}
+          </Link>
+        </Grid>
       </Grid>
       {roomList && <ListView items={roomList} />}
     </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,35 @@
-import React, { Component }  from 'react';
-import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
-import { Navbar } from '../components';
-import { Home, Login, SignupSteps, HotelBooking, RoomList, QuestionVerification, CipherVerification } from '../pages';
+import { AmplifyChatbot } from "@aws-amplify/ui-react/legacy";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import Fab from "@mui/material/Fab";
+import { Amplify } from "aws-amplify";
+import React, { useState } from "react";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { Navbar } from "../components";
+import {
+  CipherVerification,
+  Home,
+  HotelBooking,
+  Login,
+  QuestionVerification,
+  RoomList,
+  SignupSteps
+} from "../pages";
 
+Amplify.configure({
+  Auth: {
+    identityPoolId: "us-east-1:466de39b-520b-4df9-af4b-72a7d70019ee", // (required) Identity Pool ID
+    region: "us-east-1", // (required) Identity Pool region
+  },
+  Interactions: {
+    bots: {
+      RoomBookingTrial: {
+        name: "RoomBookingTrial",
+        alias: "devTrial",
+        region: "us-east-1",
+      },
+    },
+  },
+});
 
 const AppRoutes = () => {
   return (
@@ -10,10 +37,16 @@ const AppRoutes = () => {
       <Route element={<WithoutNavbar />}>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignupSteps />} />
+
+        <Route
+          path="/questionverification"
+          element={<QuestionVerification />}
+        />
+        <Route path="/cipherVerification" element={<CipherVerification />} />
+      </Route>
+      <Route element={<WithNavbar />}>
         <Route path="/hotel" element={<HotelBooking />} />
         <Route path="/rooms" element={<RoomList />} />
-        <Route path="/questionverification" element={<QuestionVerification />} />
-        <Route path="/cipherVerification" element={<CipherVerification />} />
       </Route>
       <Route
         path="*"
@@ -49,11 +82,30 @@ const RequireAuth = ({ children }) => {
 };
 
 const WithNavbar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <>
+    <div>
       <Navbar />
+      <div>
+        <Fab
+          color="primary"
+          aria-label="add"
+          className="chatButton"
+          onClick={() => setShow((prev) => !prev)}
+        >
+          <ChatOutlinedIcon />
+        </Fab>
+        {show && (
+          <AmplifyChatbot
+            botName="RoomBookingTrial"
+            botTitle="B &amp; B Assistant Support"
+            welcomeMessage="How can I help you?"
+          />
+        )}
+      </div>
+
       <Outlet />
-    </>
+    </div>
   );
 };
 
