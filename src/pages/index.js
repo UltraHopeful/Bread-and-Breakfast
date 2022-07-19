884ee33bc5b20ea78a3a815714e6aac2de9ec08d
Manan Amin <mn959427@dal.ca>
2022-07-19 06:01:36 -0300

    resolved conflict error

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Login from './login';
+import SignupSteps from './signup';
+import Home from './home';
+import HotelBooking from './hotel';
+import RoomList from './rooms';
+import QuestionVerification from './login/authenticatequestions';
+import CipherVerification from './login/cipherVerification';
+import Kitchen from './kitchen';
+import MealList from './meals';
+
+export {
+  Login,
+  SignupSteps,
+  Home,
+  HotelBooking,
+  RoomList,
+  QuestionVerification,
+  CipherVerification,
+  Kitchen,
+  MealList,
+};
